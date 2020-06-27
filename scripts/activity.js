044af3672b3bfdc8a86a62c5eabad81d520e949f
prashantchanne12 <prashantchanne121@gmail.com>
2020-06-27 15:03:33 +0530

    activity UI finished

---
+++ scripts/activity.js
@@ -0,0 +1,5 @@
+const username = document.querySelector('#username');
+const detail = document.querySelector('#detail');
+const profileImg = document.querySelector('#activity>img');
+const timestamp = document.querySelector('#timestamp');
+const img = document.querySelector('.info-img>img');
