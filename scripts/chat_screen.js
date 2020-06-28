5e37f158bd0793072b9007c43fa01535f0f98ce3
prashantchanne12 <prashantchanne121@gmail.com>
2020-06-28 20:12:05 +0530

    chat functions added

--- scripts/chat_screen.js
+++ scripts/chat_screen.js
@@ -1,37 +1,145 @@
 const form = document.querySelector('form');
 const user = JSON.parse(localStorage.getItem('currentUser'));
-let user1 = false;
-let user2 = false;
+const receiverId = localStorage.getItem('current_profile');
+const senderId = user.id;
+let chatId = '';
+let sender = false;
+let receiver = false;
+let getData = undefined;
+let data = undefined;
+
+// CHECK IF CHAT TILES EXISTS
+checkIfSenderExisted(issender => {
+    sender = issender;
+    checkIfRecieverExisted(isReciver => {
+        receiver = isReciver;
+        getData = sender && receiver;
+    });
+});
+
+// GET CHAT ID
+getChatId();
+
+// GET RECIEVRS DETAILS
+getReciverDetails();
 
 form.addEventListener('submit', e => {
     e.preventDefault();
+    const message = form.chat.value.trim();
+    if (message.length == 0) {
+        return;
+    }
+    if (getData) {
+        db.collection('messages')
+            .doc(chatId)
+            .collection('chats')
+            .add({
+                'text': message,
+                'userId': senderId,
+                'receiverId': receiverId,
+                'isSeen': false,
+                'time': firebase.firestore.Timestamp.fromDate(new Date())
+            }).then(() => {
+                console.log('message sent');
+            }).catch(e => {
+                console.log('Error: ', e);
+            });
+    } else {
 
-    // CHECK IF CHAT TILES EXISTS
-    checkChatTilesExists();
+        db.collection('chat_tiles')
+            .doc(senderId)
+            .collection('chat_users')
+            .doc(receiverId)
+            .set({
+                'id': receiverId,
+                'time': firebase.firestore.Timestamp.fromDate(new Date()),
+                'photoUrl': data.photoUrl,
+                'username': data.username,
+            }).then(() => {
+                console.log('receiver added');
+            }).catch(e => {
+                console.log('Error: ', e);
+            });
 
-    console.log('send');
+        db.collection('chat_tiles')
+            .doc(receiverId)
+            .collection('chat_users')
+            .doc(senderId)
+            .set({
+                'id': senderId,
+                'time': firebase.firestore.Timestamp.fromDate(new Date()),
+                'photoUrl': user.photoUrl,
+                'username': user.username,
+            }).then(() => {
+                console.log('sender added');
+            }).catch(e => {
+                console.log('Error: ', e);
+            });
+
+        db.collection('messages')
+            .doc(chatId)
+            .collection('chats')
+            .add({
+                'text': message,
+                'userId': senderId,
+                'receiverId': receiverId,
+                'isSeen': false,
+                'time': firebase.firestore.Timestamp.fromDate(new Date()),
+            }).then(() => {
+                console.log('message sent');
+            }).catch(e => {
+                console.log('Error: ', e);
+            });
+
+        getData = true;
+
+    }
+
+    form.reset();
+
 });
 
-function addshit() {
+function checkIfSenderExisted(callback) {
     db.collection('chat_tiles')
-        .doc('shit')
-        .set({
-            hello: 123,
-        }).then(() => {
-
+        .doc(senderId)
+        .get()
+        .then(document => {
+            callback(document.exists);
         }).catch(e => {
-            console.log('error');
+            console.log('Error: ', e);
         });
 }
 
-function checkChatTilesExists() {
+function checkIfRecieverExisted(callback) {
     db.collection('chat_tiles')
-        .doc()
+        .doc(receiverId)
         .get()
         .then(document => {
-            console.log(document);
-            console.log(document.exists);
+            callback(document.exists);
         }).catch(e => {
-            console.log('Error: ', e)
+            console.log('Error: ', e);
+        });
+}
+
+function getChatId() {
+    // we are appending smaller to greater
+    // -1 : first string is smaller
+    //  1 : first string is greater and 2nd is smaller
+    //  0 : both are equal
+    if (senderId.localeCompare(receiverId) == -1) {
+        chatId = senderId + receiverId;
+    } else {
+        chatId = receiverId + senderId;
+    }
+}
+
+function getReciverDetails() {
+    db.collection('users')
+        .doc(receiverId)
+        .get()
+        .then(document => {
+            data = document.data();
+        }).catch(e => {
+            console.log('Error: ', e);
         });
 }
